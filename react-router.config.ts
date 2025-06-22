b67a015188569b9593db69ad4361c7326c2ce399
SeanHacmon <seanhacmon@gmail.com>
2025-06-22 19:45:19 +0300

    finished github io

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,4 +2,5 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   ssr: false,
+  basename: "/Grocery-App",
 } satisfies Config;
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  // base: "/Grocery-App/",
+  base: "/Grocery-App/",
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   build: {
     outDir: "build/client",
